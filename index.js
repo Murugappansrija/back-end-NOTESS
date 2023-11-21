e575615832e7a45ebc9ed3d4f23b15c9721057fc
murugappansrija <srijamurugappan66@gmail.com>
2023-11-21 12:32:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const {mongooseConnection} = require("./common/dbConfig")
+const user = require("./Routes/user")
+const note = require("./Routes/note")
+const dotenv = require ('dotenv')
+dotenv.config()
+
+mongooseConnection()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/user",user)
+app.use("/note",note)
+
+
+app.listen(process.env.PORT,console.log(`Server is runing on ${process.env.PORT} PORT`))
